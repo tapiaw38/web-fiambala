7958ca5e2fe18bf77593931d0ad25894a52f117c
tapiaw38 <tapiaw38@gmail.com>
2020-01-24 16:17:10 +0100

    update3

--- static/js/animaciones.js
+++ static/js/animaciones.js
@@ -10,6 +10,23 @@ function apareceScroll(){
 				elementoAparece[i].style.opacity = 1;
 			}
 		}
-	})
+	});
 };
 apareceScroll();
+
+$(document).ready(function(){
+	$('.ecirculo').hide().fadeIn(600);
+});
+
+
+function iniciarMap(){
+    var coord = {lat:-27.6849696 ,lng:-67.6589818};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
